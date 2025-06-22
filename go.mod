a02f364f1748da0a31bf584254db1e8f9281eaff
Alex <alex.manson14@gmail.com>
2025-06-22 22:26:57 +0100

    Added munros db

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AlexM141200/munros-api
 
 go 1.22.5
+
+require github.com/mattn/go-sqlite3 v1.14.28 // indirect
--- src/api/routes.go
+++ src/api/routes.go
@@ -3,14 +3,11 @@ package api
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/AlexM141200/munros-api/src/model"
+	//	"github.com/AlexM141200/munros-api/src/model"
 )
 
 func handleGetMunros(w http.ResponseWriter, r *http.Request) {
-	munro := model.Munro{Id: "1", Name: "Ben Nevis", Height: 1245.0, Location: "Grampian Mountains"}
-	res := fmt.Sprintf("Munro Details: ID= %s, Name=%s, Height=%f, Location=%s", munro.Id, munro.Name, munro.Height, munro.Location)
-	w.Write([]byte(res))
+
 }
 
 func handleMunroByID(w http.ResponseWriter, r *http.Request) {
